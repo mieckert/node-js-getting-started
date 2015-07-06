1a8c6ca6833ff43ff0ee058534ab207ef033ea19
Michael Eckert <meckert@salesforce.com>
2015-07-06 16:31:38 +0200

    Added postgres code to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -24,3 +25,17 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/db', function(request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if(err) {
+                console.error(err);
+                response.send("Error: " + err);
+            }
+            else {
+                response.render('pages/db', {results: result:rows});
+            }
+        });
+    }); 
+});
