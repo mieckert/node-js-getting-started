ea1a73ff98203845387aabc0c5eb86f0325c75ee
Michael Eckert <meckert@salesforce.com>
2015-07-06 16:01:26 +0200

    Demo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,11 +11,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+    response.send(cool());
+    // response.render('pages/index')
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
 
